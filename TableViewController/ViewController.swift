67e92d7c7c069b377bd0223d32cc6ab1358dcfc2
HmlPtl <cdthemal@yahoo.com>
2014-08-24 14:19:42 +0530

    TableViewController
    Simple TableViewController using swift

--- TableViewController/ViewController.swift
+++ TableViewController/ViewController.swift
@@ -8,10 +8,20 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController ,UITableViewDataSource,UITableViewDelegate{
                             
     override func viewDidLoad() {
         super.viewDidLoad()
+        var tvc  = UITableView()
+        let ds = self
+        tvc.delegate = self
+        tvc.dataSource = self
+        tvc.frame = self.view.bounds
+        
+        self.view .addSubview(tvc)
+        self.view.backgroundColor = UIColor(red: 1, green: 0, blue: 0, alpha: 1)
+        
+        //tvc.registerClass(UITableViewCell.self, forCellReuseIdentifier: "cell")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,7 +29,25 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func tableView(tableView: UITableView!, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(tableView: UITableView!, cellForRowAtIndexPath indexPath: NSIndexPath!) -> UITableViewCell! {
+        
+        var cell:UITableViewCell = UITableViewCell(style:.Value2, reuseIdentifier: "cell")
+        let index = indexPath!.row;
+        cell.textLabel.text = "Cell Index - \(index)"
+        //cell.image = UIImage(named:"img.png")
+        
+        return cell
+    }
+    
+    func tableView(tableView: UITableView!, didSelectRowAtIndexPath indexPath: NSIndexPath!) {
+        let index = indexPath!.row;
+        println("Cell Index - \(index)")
+    }
 
 }
 
